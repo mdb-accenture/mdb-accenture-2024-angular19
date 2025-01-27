5d6c16a688dfbf463b435e8fae4fc84d225139cb
mdb-accenture <mark.daniel.bandilla@accenture.com>
2025-01-27 09:57:34 +0800

    Design: Update Contact Cards & Table

--- src/app/components/contact-cards/contact-cards.component.ts
+++ src/app/components/contact-cards/contact-cards.component.ts
@@ -3,11 +3,12 @@ import { RouterLink } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { ContactMock, IContact } from '../../../model/contact';
 
 @Component({
   selector: 'app-contact-cards',
-  imports: [MatCardModule, MatButtonModule, MatIconModule, RouterLink],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatToolbarModule, RouterLink],
   templateUrl: './contact-cards.component.html',
   styleUrl: './contact-cards.component.css'
 })
