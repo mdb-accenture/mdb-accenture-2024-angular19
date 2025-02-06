4ffa96f8280966b1dd7573756c5c6e06494670ce
mdb-accenture <mark.daniel.bandilla@accenture.com>
2025-02-06 17:24:31 +0800

    IMPL: Add Mask Pipe

---
+++ src/app/pipes/mask.pipe.ts
@@ -0,0 +1,31 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'mask'
+})
+export class MaskPipe implements PipeTransform {
+
+  transform(value: string, mask: string): string {
+    if (!value.length) return '';
+    value = value.replace(/\D+/g, "");
+
+    let maskedValue = '';
+    let valueIndex = 0;
+
+    for (let i = 0; i < mask.length; i++) {
+      if (mask[i] === '#') {
+        if (value[valueIndex]) {
+          maskedValue += value[valueIndex];
+          valueIndex++;
+        } else {
+          break;
+        }
+      } else {
+        maskedValue += mask[i];
+      }
+    }
+
+    return maskedValue;
+  }
+
+}
