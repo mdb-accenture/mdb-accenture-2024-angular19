9ae1be1731cfd6ef38f2a5cbf281f15c0ee17920
mdb-accenture <mark.daniel.bandilla@accenture.com>
2025-02-06 13:14:39 +0800

    IMPL: Update Contact Model

---
+++ src/app/models/contact.ts
@@ -0,0 +1,31 @@
+export interface IContact {
+    id: number | string;
+    name: string;
+    contactNumber: string;
+    emailAddress: string;
+}
+
+export interface IContactAction {
+    contact?: IContact;
+    action: string;
+}
+
+export class Contact<IContact> {
+    id: number = 0;
+    name: string = "";
+    contactNumber: string = "";
+    emailAddress: string = "";
+}
+
+export const ContactMock: IContact[] = [
+    { id: 1, name: "Mark Bandilla", contactNumber: "0905-777-8910", emailAddress: "fake_email@gmail.com" },
+    { id: 2, name: "Jay Contreras", contactNumber: "0917-123-4567", emailAddress: "kamikazeegod@gmail.com" },
+    { id: 3, name: "Jason Astete", contactNumber: "0920-600-2222", emailAddress: "jayson_the_manace@gmail.com" },
+    { id: 4, name: "Mikki Hill", contactNumber: "0920-600-2222", emailAddress: "keyboardista@gmail.com" },
+    { id: 5, name: "Jose Luis Linao", contactNumber: "0917-123-4456", emailAddress: "kamikazeeprince@gmail.com" },
+    { id: 6, name: "Allan Burdeos", contactNumber: "0917-666-6666", emailAddress: "allan_burdeos@gmail.com" },
+    { id: 7, name: "Mark Estacio", contactNumber: "0917-666-6666", emailAddress: "mark_estacio@gmail.com" },
+    { id: 8, name: "Jianelli Lubiano", contactNumber: "0917-888-7777", emailAddress: "kamikazeeprincess@gmail.com" },
+    { id: 9, name: "Led Zeppelin Tuyay", contactNumber: "0917-444-1234", emailAddress: "led_zt@gmail.com" },
+    { id: 10, name: "Sep Rono", contactNumber: "0917-444-1234", emailAddress: "sep_of_typecast@gmail.com" },
+];
