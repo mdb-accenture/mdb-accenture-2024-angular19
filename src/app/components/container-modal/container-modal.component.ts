647e390e224811acafd11a5532bda03e1132e2a8
mdb-accenture <mark.daniel.bandilla@accenture.com>
2025-02-07 16:34:22 +0800

    IMPL: Add Modal Container

---
+++ src/app/components/container-modal/container-modal.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container-modal',
+  imports: [],
+  templateUrl: './container-modal.component.html',
+  styleUrl: './container-modal.component.css'
+})
+export class ContainerModalComponent {}
